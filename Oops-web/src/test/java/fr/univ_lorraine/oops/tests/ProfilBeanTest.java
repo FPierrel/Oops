971c82a221580970d1241c1435dec1e51f504f1a
Geoffrey <Geoffrey@DESKTOP-PL40E9E>
2016-01-08 15:24:19 +0100

    Corrections tests édition profil.

--- Oops-web/src/test/java/fr/univ_lorraine/oops/tests/ProfilBeanTest.java
+++ Oops-web/src/test/java/fr/univ_lorraine/oops/tests/ProfilBeanTest.java
@@ -1,12 +1,6 @@
 package fr.univ_lorraine.oops.tests;
 
-import fr.univ_lorraine.oops.library.model.Adresse;
-import fr.univ_lorraine.oops.library.model.Soumissionnaire;
-import fr.univ_lorraine.oops.library.model.Utilisateur;
-import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -21,49 +15,24 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class ProfilBeanTest {
 
-    @PersistenceContext(unitName = "fr.univ_lorraine_Oops-library_jar_1.0-SNAPSHOTPU")
-    private static EntityManager em;
-    private static Utilisateur userTest;
     private static WebDriver driver;
 
     public ProfilBeanTest() {
     }
 
-    public static void createUserTest() {
-        ArrayList<Adresse> adressesTest = new ArrayList<>();
-        Adresse adresseTest = new Adresse();
-        adresseTest.setNumero("1664");
-        adresseTest.setRue("rue du test");
-        adresseTest.setVille("PARIS");
-        adresseTest.setCodePostal("75000");
-        adresseTest.setPays("FRANCE");
-        adressesTest.add(adresseTest);
-        userTest = new Soumissionnaire();
-        userTest.setLogin("test1234567890");
-        userTest.setAdresses(adressesTest);
-        userTest.setMail("test@test.com");
-        userTest.setMotDePasse("8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92");
-        userTest.setNom("Test");
-        userTest.setPrenom("User");
-        userTest.setNumeroTelephone("0123456789");
-        em.persist(userTest);
-    }
-
     @BeforeClass
     public static void setUpClass() {
-        createUserTest();
         DesiredCapabilities capabilities = DesiredCapabilities.phantomjs();
         driver = new PhantomJSDriver(capabilities);
         driver.manage().window().setSize(new Dimension(1440, 768));
         driver.get("http://localhost:8080/Oops-web/faces/profil.xhtml");
-        driver.findElement(By.id("loginForm:username")).sendKeys("test1234567890");
+        driver.findElement(By.id("loginForm:username")).sendKeys("john");
         driver.findElement(By.id("loginForm:password")).sendKeys("123456");
         driver.findElement(By.id("loginForm:login")).click();
     }
 
     @AfterClass
     public static void tearDownClass() {
-        em.remove(userTest);
         driver.quit();
     }
 
