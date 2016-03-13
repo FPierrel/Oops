6d43f570418f6543b1608910ded7d24abb661dc4
rbailloux <gloutoni@free.fr>
2016-03-13 20:43:27 +0100

    Minor fix

--- Oops-web/src/main/java/fr/univ_lorraine/oops/beans/SearchBean.java
+++ Oops-web/src/main/java/fr/univ_lorraine/oops/beans/SearchBean.java
@@ -43,12 +43,11 @@ public class SearchBean implements Serializable {
         String ville = "";
         String codePostal = "";
         
-        if (this.choix.split(" ").length == 1) {
-            return "results?faces-redirect=true";
-        }
         if(this.choix != null && !this.choix.isEmpty()) {
             ville = this.choix.split(" ")[0];
-            codePostal = this.choix.split(" ")[1].replaceAll("[()]", "");
+            if (this.choix.split(" ").length > 1) {
+                codePostal = this.choix.split(" ")[1].replaceAll("[()]", "");
+            }
         }
 
         if (this.advanced) {
