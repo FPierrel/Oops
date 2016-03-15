ee30c3c15e465143cedb6a0fdbde6d79fe65ab59
GDesanlis <geoffreydesanlis@gmail.com>
2016-03-15 15:56:56 +0100

    Javadoc encore et toujours.

--- Oops-ejb/src/main/java/fr/univ_lorraine/oops/dal/CategorieDAL.java
+++ Oops-ejb/src/main/java/fr/univ_lorraine/oops/dal/CategorieDAL.java
@@ -18,24 +18,53 @@ public class CategorieDAL {
     @PersistenceContext(unitName = "fr.univ_lorraine_Oops-library_jar_1.0-SNAPSHOTPU")
     private EntityManager em;
 
+    /**
+     * Méthode permettant d'ajouter une catégorie.
+     *
+     * @param c : catégorie à ajouter.
+     * @return la catégorie ajoutée.
+     */
     public Categorie add(Categorie c) {
         em.persist(c);
         return c;
     }
 
+    /**
+     * Méthode donnant une catégorie à partir de son identifiant.
+     *
+     * @param id : identifiant de la cétégorie voulue.
+     * @return la catégorie voulue.
+     */
     public Categorie get(Long id) {
         return em.find(Categorie.class, id);
     }
 
+    /**
+     * Méthode donnant toutes les catégories.
+     *
+     * @return la liste de toutes les catégories.
+     */
     public List<Categorie> getAll() {
         String query = "SELECT c FROM Categorie c";
         return em.createQuery(query).getResultList();
     }
 
+    /**
+     * Méthode permettant de mettre à jour une catégorie.
+     *
+     * @param c : catégorie à mettre à jour.
+     * @return la catégorie mise à jour.
+     */
     public Categorie update(Categorie c) {
         return em.merge(c);
     }
 
+    /**
+     * Méthode donnant la liste des catégories à partir de leur nom.
+     *
+     * @param l : liste de noms de catégories voulues.
+     * @return la liste des catégories voulues.
+     */
     public Collection<Categorie> getFromList(List<String> l) {
         Collection<Categorie> c = new ArrayList<>();
         for (String s : l) {
@@ -47,12 +76,23 @@ public class CategorieDAL {
         return c;
     }
 
+    /**
+     * Méthode donnant la catégorie parente.
+     *
+     * @return la catégorie parente.
+     */
     public Categorie getCategories() {
         Query query = em.createNamedQuery("Categorie.findByName");
         query.setParameter("name", "Toutes cat\u00e9gories");
         return (Categorie) query.getSingleResult();
     }
 
+    /**
+     * Méthode donnant la liste des catégories d'un prestataire.
+     *
+     * @param login : login du prestataire.
+     * @return la liste des catégories du prestataire.
+     */
     public List<Categorie> getCategoriesOfPrestataire(String login) {
         String queryString = "SELECT c "
                 + "FROM Prestataire p, Categorie c "
--- Oops-ejb/src/main/java/fr/univ_lorraine/oops/dal/CommentaireDAL.java
+++ Oops-ejb/src/main/java/fr/univ_lorraine/oops/dal/CommentaireDAL.java
@@ -1,6 +1,5 @@
 package fr.univ_lorraine.oops.dal;
 
-import fr.univ_lorraine.oops.library.model.Avis;
 import fr.univ_lorraine.oops.library.model.Commentaire;
 import java.util.List;
 import javax.ejb.LocalBean;
@@ -14,27 +13,55 @@ public class CommentaireDAL {
 
     @PersistenceContext(unitName = "fr.univ_lorraine_Oops-library_jar_1.0-SNAPSHOTPU")
     private EntityManager em;
-    
-    public Commentaire add(Commentaire c){
+
+    /**
+     * Méthode permettant d'ajouter un commentaire.
+     *
+     * @param c : commentaire à ajouter.
+     * @return le commentaire ajouté.
+     */
+    public Commentaire add(Commentaire c) {
         em.persist(c);
         return c;
     }
-    
-    public Commentaire get(Long id){
+
+    /**
+     * Méthode donnant un commentaire à partie de son identifiant.
+     *
+     * @param id : identifiant du commentaire voulu.
+     * @return le commentaire voulu.
+     */
+    public Commentaire get(Long id) {
         return em.find(Commentaire.class, id);
     }
-    
-    public List<Commentaire> getAll(){
+
+    /**
+     * Méthode donnant la liste de tous les commentaires.
+     *
+     * @return la liste de tous les commentaires.
+     */
+    public List<Commentaire> getAll() {
         String query = "SELECT c FROM Commentaire c";
         return em.createQuery(query).getResultList();
     }
-    
-    public Commentaire update(Commentaire c){
+
+    /**
+     * Méthode permettant de mettre à jour un commentaire.
+     *
+     * @param c : commentaire à mettre à jour.
+     * @return le commentaire mis à jour.
+     */
+    public Commentaire update(Commentaire c) {
         return em.merge(c);
-    }  
-    
-    public void delete(Commentaire c){
-        em.remove(em.find(Commentaire.class,c.getId()));
-        
+    }
+
+    /**
+     * Méthode permettant de supprimmer un commentaire.
+     *
+     * @param c : commentaire à supprimmer.
+     */
+    public void delete(Commentaire c) {
+        em.remove(em.find(Commentaire.class, c.getId()));
+
     }
 }
