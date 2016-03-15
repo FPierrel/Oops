77b6eb81e3c5dd6c20c28e849bc773fdcf4adc7c
GDesanlis <geoffreydesanlis@gmail.com>
2016-03-15 15:47:27 +0100

    Suite suite javadoc.

--- Oops-ejb/src/main/java/fr/univ_lorraine/oops/dal/AlbumDAL.java
+++ Oops-ejb/src/main/java/fr/univ_lorraine/oops/dal/AlbumDAL.java
@@ -14,31 +14,65 @@ public class AlbumDAL {
 
     @PersistenceContext(unitName = "fr.univ_lorraine_Oops-library_jar_1.0-SNAPSHOTPU")
     private EntityManager em;
-    
-    public Album add(Album a){
+
+    /**
+     * Méthode permettant d'ajouter un album.
+     *
+     * @param a : album à ajouter.
+     * @return l'album ajouté.
+     */
+    public Album add(Album a) {
         em.persist(a);
         return a;
     }
-    
-    public Album get(Long id){
+
+    /**
+     * Méthode donnant un album à partir de son identifiant.
+     *
+     * @param id : identifiant de l'album voulu.
+     * @return album voulu.
+     */
+    public Album get(Long id) {
         return em.find(Album.class, id);
     }
-    
-    public List<Album> getAll(){
+
+    /**
+     * Méthode donnant tous les albums.
+     *
+     * @return liste de tous les albums.
+     */
+    public List<Album> getAll() {
         String query = "SELECT a FROM Soumissionnaire a";
         return em.createQuery(query).getResultList();
     }
-    
-    public Album update(Album a){
+
+    /**
+     * Méthode permettant de mettre à jour un album.
+     *
+     * @param a : album à mettre à jour.
+     * @return album mis à jour.
+     */
+    public Album update(Album a) {
         return em.merge(a);
-    }  
-    
-    public void delete(Album a){
+    }
+
+    /**
+     * Méthode permettant de supprimmer un album.
+     *
+     * @param a : album à supprimer.
+     */
+    public void delete(Album a) {
         em.remove(a);
     }
-    
-    public List<Photo> photosOfAlbum(Long id){
+
+    /**
+     * Méthode donnant toutes les photos d'un album.
+     *
+     * @param id : identifiant de l'album.
+     * @return la liste des photos de l'album.
+     */
+    public List<Photo> photosOfAlbum(Long id) {
         Album a = em.find(Album.class, id);
         return (List<Photo>) a.getPhotos();
-}
+    }
 }
--- Oops-ejb/src/main/java/fr/univ_lorraine/oops/dal/AvisDAL.java
+++ Oops-ejb/src/main/java/fr/univ_lorraine/oops/dal/AvisDAL.java
@@ -14,20 +14,42 @@ public class AvisDAL {
     @PersistenceContext(unitName = "fr.univ_lorraine_Oops-library_jar_1.0-SNAPSHOTPU")
     private EntityManager em;
 
+    /**
+     * Méthode permettant d'ajouter un avis.
+     *
+     * @param a : avis à ajouter.
+     * @return l'avis ajouté.
+     */
     public Avis add(Avis a) {
         em.persist(a);
         return a;
     }
 
+    /**
+     * Méthode donnat un avis à partie de son identifiant.
+     *
+     * @param id : identifiant de l'avis voulu.
+     * @return l'avis voulu.
+     */
     public Avis get(Long id) {
         return em.find(Avis.class, id);
     }
 
+    /**
+     * Méthode donnant tous les avis.
+     *
+     * @return la liste de tous les avis.
+     */
     public List<Avis> getAll() {
         String query = "SELECT a FROM Avis a";
         return em.createQuery(query).getResultList();
     }
 
+    /**
+     * Méthode donnant la liste des avis non vérifiés.
+     *
+     * @return la liste des avis non vérifiés.
+     */
     public List<Avis> getAllUnverified() {
         String queryString = "SELECT a "
                 + "FROM Avis a "
@@ -35,14 +57,31 @@ public class AvisDAL {
         return em.createQuery(queryString, Avis.class).getResultList();
     }
 
+    /**
+     * Méthode permettant de mettre à jour un avis.
+     *
+     * @param a : avis à mettre à jour.
+     * @return l'avis mis à jour.
+     */
     public Avis update(Avis a) {
         return em.merge(a);
     }
 
+    /**
+     * Méthode permettant de supprimmer un avis.
+     *
+     * @param a : avis à supprimmé.
+     */
     public void delete(Avis a) {
         em.remove(em.find(Avis.class, a.getId()));
     }
 
+    /**
+     * Méthode donnant tous les avis sur un prestataire.
+     *
+     * @param loginPrestataire : login du prestataire.
+     * @return la liste des avis concernant le prestataire.
+     */
     public List<Avis> getAvisPrestatire(String loginPrestataire) {
         String queryString = "SELECT p.cAvis "
                 + "FROM Prestataire p "
@@ -50,6 +89,12 @@ public class AvisDAL {
         return em.createQuery(queryString, Avis.class).getResultList();
     }
 
+    /**
+     * Méthode donnant les derniers avis pour la page d'accueil.
+     *
+     * @param number : nombre voulu d'avis parmi les derniers.
+     * @return la liste des derniers avis.
+     */
     public List<Avis> getLastAvis(int number) {
         String query = "SELECT a FROM Avis a ORDER BY a.id desc";
         return em.createQuery(query).setMaxResults(number).getResultList();
