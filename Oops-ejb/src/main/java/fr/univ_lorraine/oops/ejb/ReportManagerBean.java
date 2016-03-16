90603e058bb3d44e12cab9d458d7956973fa71a6
thomas <thomas@thomas-N53SV>
2016-03-16 17:32:16 +0100

    Correction incompétence de Romain.

--- Oops-ejb/src/main/java/fr/univ_lorraine/oops/ejb/ReportManagerBean.java
+++ Oops-ejb/src/main/java/fr/univ_lorraine/oops/ejb/ReportManagerBean.java
@@ -48,7 +48,7 @@ public class ReportManagerBean {
      * @param complement the complementary reason of the report 
      */
     public void reportPhoto(String loginReporting, Photo photo, Album album, String reason, String complement) {
-        String loginFicheReported = loginReporting;
+        String loginFicheReported = album.getLogin();
         Prestataire pres = pd.get(loginFicheReported);
         ReportPhoto report = new ReportPhoto();
         report.setReason(reason);
